b887bb74237c1816dac025107703688128525c84
Zhenya Khomich <zhenyatsoy21061992@gmail.com>
2025-08-27 08:59:31 +0400

    itit: initial project and create basic structure

---
+++ frontend/src/router.js
@@ -0,0 +1,88 @@
+import {Dashboard} from "./components/dashboard.js";
+import {Login} from "./components/login.js";
+import {SignUp} from "./components/sign-up.js";
+import response from "admin-lte/plugins/jszip/jszip";
+
+export class Router {
+    constructor() {
+        this.titlePageElement = document.getElementById('title');
+        this.contentPageElement = document.getElementById('content');
+
+        this.initEvents();
+
+        this.routes = [
+            {
+                route: '/',
+                title: 'Дашборд',
+                filePathTemplate: '/templates/dashboard.html',
+                useLayout: '/templates/layout.html',
+                load: () => {
+                    new Dashboard();
+                }
+            },
+            {
+                route: '/404',
+                title: 'Страница не найдена',
+                filePathTemplate: '/templates/404.html',
+                useLayout: false,
+            },
+            {
+                route: '/login',
+                title: 'Авторизация',
+                filePathTemplate: '/templates/login.html',
+                useLayout: false,
+                load: () => {
+                    new Login();
+                }
+            },
+            {
+                route: '/sing-up',
+                title: 'Регистрация',
+                filePathTemplate: '/templates/sign-up.html',
+                useLayout: false,
+                load: () => {
+                    new SignUp();
+                }
+            },
+        ]
+    }
+
+    initEvents() {
+        window.addEventListener('DOMContentLoaded', this.activateRoute.bind(this));
+        window.addEventListener('popstate', this.activateRoute.bind(this));
+    }
+
+    async activateRoute() {
+        const urlRoute = window.location.pathname;
+        const newRoute = this.routes.find(item => item.route === urlRoute);
+
+        if (newRoute) {
+            if (newRoute.title) {
+                this.titlePageElement.innerHTML = newRoute.title + '| Freelance Studio';
+            }
+
+            if (newRoute.filePathTemplate) {
+                let contentBlock = this.contentPageElement;
+
+                if (newRoute.useLayout) {
+                    this.contentPageElement.innerHTML = await fetch(newRoute.useLayout).then(response => response.text());
+                    contentBlock = document.getElementById('content-layout');
+                    document.body.classList.add('sidebar-mini');
+                    document.body.classList.add('layout-fixed');
+                } else {
+                    document.body.classList.remove('sidebar-mini');
+                    document.body.classList.remove('layout-fixed');
+                }
+
+                contentBlock.innerHTML = await fetch(newRoute.filePathTemplate).then(response => response.text());
+            }
+
+            if (newRoute.load && typeof newRoute.load === 'function') {
+                newRoute.load();
+            }
+        } else {
+            console.log('No route found');
+            window.location = '/404';
+        }
+    }
+}
